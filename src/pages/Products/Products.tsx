4a9414cfdb6c5004100ba3ccd36736ef23d83e09
DevAlper35 <devcalisma@gmail.com>
2024-08-05 02:21:07 +0300

    Edit data name

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -5,20 +5,20 @@ import axios from 'axios';
 import ProductCard from '../../components/ProductCard';
 
 export default function Products() {
-    const [data, setData] = useState([]);
+    const [productData, setProductData] = useState([]);
     const API_URL= "https://fakestoreapi.com/products"
     useEffect(() =>{
         fetchData()
     }, []) 
 
     const fetchData = async() => {
-        const {data: productData} = await axios.get(API_URL)
-        setData(productData)
+        const {data} = await axios.get(API_URL)
+        setProductData(data)
     }
     const renderProduct = ({item}) => <ProductCard product={item}/>
     return (
         <View>
-        <FlatList data={data} renderItem={renderProduct}/>
+        <FlatList data={productData} renderItem={renderProduct}/>
         </View>
     )
 }
