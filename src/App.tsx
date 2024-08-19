e59747cd38ac212f96c86d399cdcd8d040adfbed
Alper MUDURLU <152246898+DevAlper35@users.noreply.github.com>
2024-08-19 22:17:30 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import {NavigationContainer} from "@react-navigation/native"
-import {createNativeStackNavigator} from "@react-navigation/native-stack"
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
 import Products from './pages/Products';
 import Detail from './pages/Detail';
@@ -11,25 +11,25 @@ import Login from './pages/Login';
 const Stack = createNativeStackNavigator()
 
 function App(): React.JSX.Element {
-  
-  
+
+
 
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerTintColor: "white",headerStyle: {backgroundColor: "cornflowerblue"}}}>
+      <Stack.Navigator screenOptions={{ headerTintColor: "white", headerStyle: { backgroundColor: "cornflowerblue" } }}>
         <Stack.Screen name='Login' component={Login} options={{
-          title : "Dükkan", headerShown:false
-        }}/>
+          title: "Dükkan", headerShown: false
+        }} />
         <Stack.Screen name='ProductPage' component={Products} options={{
-          title : "Dükkan",
-        }}/>
-        <Stack.Screen name='Detail' component={Detail} 
+          title: "Dükkan",
+        }} />
+        <Stack.Screen name='Detail' component={Detail}
           options={{
             /*headerShown:false*/
-            title : "Ürün",
-            headerStyle: {backgroundColor: "cornflowerblue",},
+            title: "Ürün",
+            headerStyle: { backgroundColor: "cornflowerblue", },
             headerTintColor: "white"
-            }}/>
+          }} />
       </Stack.Navigator>
     </NavigationContainer>
   )
